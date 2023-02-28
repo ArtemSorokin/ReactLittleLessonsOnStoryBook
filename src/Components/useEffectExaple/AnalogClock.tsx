da66d579fd7568af4d23cab7c06e7a57a19637a7
ArtemSorokin <artemsorokin4395@gmail.com>
2023-02-28 23:14:23 +0300

    useEffect + Clock

--- src/Components/useEffectExaple/AnalogClock.tsx
+++ src/Components/useEffectExaple/AnalogClock.tsx
@@ -1,29 +1,62 @@
 
 
 import style from './AnalogClock.module.css'
+import {useEffect} from "react";
 
 
 
 
 
 export const AnalogClock = ()=> {
+
+
+useEffect(()=>{
+
+    debugger
+
+    const deg = 6;
+    // const hours = document.querySelector('#hours');
+    // const seconds = document.querySelector('#seconds');
+    // const minutes = document.querySelector('#minute');
+
+    setInterval(() => {
+        let day = new Date();
+        let hour = day.getHours() * 30;
+        let minute = day.getMinutes() * deg;
+        let second = day.getSeconds() * deg;
+
+// каким-то образом находи id в элементе без queryselectora
+// @ts-ignore
+        hours.style.transform = `rotateZ(${(hour) + (minute/12)}deg)`;
+        // @ts-ignore
+        minutes.style.transform = `rotateZ(${minute}deg)`;
+        // @ts-ignore
+        seconds.style.transform  = `rotateZ(${second}deg)`;
+    }, 1000)
+
+}, [])
+
+
+
+
     return <div className={style.body}>
 
 <div className={style.clock} >
 
     <div className={style.hour}>
-        <div className={style.hours}> </div>
+        <div className={style.hours} id={'hours'}> </div>
     </div>
 <div className={style.minute}>
-    <div className={style.minutes}> </div>
+    <div className={style.minutes} id={'minutes'}> </div>
 </div>
     <div className={style.second}>
-        <div className={style.seconds}> </div>
+        <div className={style.seconds} id={'seconds'}> </div>
     </div>
 
 </div>
     </div>
 }
+
 
 
 
